1b937ffbe1554c8abf50b60110ab8f72d9191291
suhanichoudhary2403 <suhanichoudhary2403@gmail.com>
2026-08-29 11:11:36 +0530

    Add files via upload

---
+++ Class_Problems/PrimeCheck.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        boolean isPrime = true;
+        if (number < 2) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        System.out.println("Is the number " + number + " a Prime number? " + isPrime);
+        scanner.close();
+    }
+}
